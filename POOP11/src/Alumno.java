3290eb4354ec836ae021379d48161d3e66a52619
TorraLeo <103598865+TorraLeo@users.noreply.github.com>
2022-11-18 00:07:12 -0600

    Co-authored-by: rat2ny <rat2ny@users.noreply.github.com>

--- POOP11/src/Alumno.java
+++ POOP11/src/Alumno.java
@@ -1,3 +1,73 @@
 public class Alumno {
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+    private int numCuenta;
+    private int edad;
+    private float promedio;
     
+    public Alumno(String nombre, String aPaterno, String aMaterno, int numCuenta, int edad, float promedio) {
+        this.nombre = nombre;
+        this.aPaterno = aPaterno;
+        this.aMaterno = aMaterno;
+        this.numCuenta = numCuenta;
+        this.edad = edad;
+        this.promedio = promedio;
+    }
+
+    public Alumno() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+    public int getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(int numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + nombre + "," + aPaterno + "," + aMaterno + "," + numCuenta + "," + edad + "," + promedio;
+    }
 }
--- POOP11/src/Principal.java
+++ POOP11/src/Principal.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import javax.swing.InputMap;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,7 +26,71 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Creación de alumnos");
+        // Alumno arreglo [] = new Alumno [10];
+        LinkedList<Alumno> alumnos = new LinkedList<>();
+        Alumno Andres = new Alumno("Andrés Eduardo", "Caamaño", "López", 123432456, 23, 9.4f);
+        Alumno Luis = new Alumno("Luis David", "Esponda", "Cervantes", 421332321, 21, 8.7f);
+        Alumno Kris = new Alumno("Kris", "Escamilla", "Suárez", 323650606, 19, 9.4f);
+        Alumno Miguel = new Alumno("Miguel", "Domínguez", "Fernández", 123432456, 23, 9.9f);
+        Alumno Antonio = new Alumno("Antonio", "Juárez", "Ortiz", 323823825, 18, 8.7f);
+        Alumno Enrique = new Alumno("Enrique", "Escamilla", "Suárez", 323515773, 19, 8.4f);
+        Alumno Andrea = new Alumno("Andrea", "Ortega", "Lara", 323148237, 20, 5.4f);
+        Alumno Lucero = new Alumno("Lucero", "Contreras", "Luna", 323237812, 22, 9.7f);
+        Alumno Jose = new Alumno("Jose", "Hernández", "Ramos", 323637661, 21, 9.0f);
+        Alumno Arantza = new Alumno("Arantza", "Castro", "Velázquez", 323216871, 18, 7.2f);
+        alumnos.add(Andres);
+        alumnos.add(Luis);
+        alumnos.add(Kris);
+        alumnos.add(Miguel);
+        alumnos.add(Antonio);
+        alumnos.add(Enrique);
+        alumnos.add(Andrea);
+        alumnos.add(Lucero);
+        alumnos.add(Jose);
+        alumnos.add(Arantza);
+        // arreglo.add(Andres);
+        // System.out.println(alumnos);
+        System.out.println("Exportar a .csv");
+        try {
+            FileWriter fw = new FileWriter("listaalumnos.csv");
+            BufferedWriter bw = new BufferedWriter(fw);
+            try (PrintWriter salida = new PrintWriter(bw)) {
+                salida.println(alumnos);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Leer del .csv");
+
+        System.out.println("FileReader===================================");
+        try {
+            FileReader fr = new FileReader("listaalumnos.csv");// apunta al archivo
+            BufferedReader bn = new BufferedReader(fr); //
+            System.out.println("El texto del archivo es: ");
+            String linea = bn.readLine();
+            while (linea != null) {
+                System.out.println(linea);
+                linea = bn.readLine();
+            }
+            System.out.println("StringTokenizer========================================");
+            StringTokenizer tokenizer = new StringTokenizer(linea, ",");
+            while (tokenizer.hasMoreTokens()) {
+                System.out.println(tokenizer.nextToken()); // impresion de elementos del csv por item
+            }
+            bn.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
+        // lo que hice es agregar los cosos a una lista ligada, para subirlos izi
+        // el pedo es regresarlos
+        // entonces no se, el string tokenizer vale pija, por lo de los [], asi que ni
+        // los puede separar
+
+        // No e
+        // e
     }
-    
+
 }
