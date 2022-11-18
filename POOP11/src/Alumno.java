76e228f7f86eb57976176be3d7d0d1fded672beb
TorraLeo <103598865+TorraLeo@users.noreply.github.com>
2022-11-18 00:13:09 -0600

    Parámetros JavaDoc

--- POOP11/src/Alumno.java
+++ POOP11/src/Alumno.java
@@ -18,54 +18,93 @@ public class Alumno {
     public Alumno() {
     }
 
+    /**
+     * @return Regresa el nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @param nombre Método que modifica el nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * @return Regresa Apellido Paterno
+     */
     public String getaPaterno() {
         return aPaterno;
     }
 
+    /**
+     * @param aPaterno Método que modifica el apellido paterno
+     */
     public void setaPaterno(String aPaterno) {
         this.aPaterno = aPaterno;
     }
 
+    /**
+     * @return Regresa apellido materno
+     */
     public String getaMaterno() {
         return aMaterno;
     }
 
+    /**
+     * @param aMaterno Método que modifica el apellido materno
+     */
     public void setaMaterno(String aMaterno) {
         this.aMaterno = aMaterno;
     }
 
+    /**
+     * @return Regresa el número de cuenta
+     */
     public int getNumCuenta() {
         return numCuenta;
     }
 
+    /**
+     * @param numCuenta Método que modifica el número de cuenta
+     */
     public void setNumCuenta(int numCuenta) {
         this.numCuenta = numCuenta;
     }
 
+    /**
+     * @return Regresa la edad
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     * @param edad Método que modifica la edad
+     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
+    /**
+     * @return Regresa el promedio
+     */
     public float getPromedio() {
         return promedio;
     }
 
+    /**
+     * @param promedio Método que modifica el promedio
+     */
     public void setPromedio(float promedio) {
         this.promedio = promedio;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     @Override
     public String toString() {
         return "\n" + nombre + "," + aPaterno + "," + aMaterno + "," + numCuenta + "," + edad + "," + promedio;
